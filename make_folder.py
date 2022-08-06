c47ff523df6a07a4e3e766669dce84e816ff527c
kaikai529 <kaishi.19990513@icloud.com>
2022-08-06 18:29:33 +0900

    update make_folder

--- make_folder.py
+++ make_folder.py
@@ -8,7 +8,7 @@ if __name__=="__main__":
 
     if os.path.exists(contest_name): exit()
     else: os.mkdir(contest_name)
-    shutil.copy("template.py", contest_name+"/"+contest_name+"_a.py")
-    shutil.copy("template.py", contest_name+"/"+contest_name+"_b.py")
-    shutil.copy("template.py", contest_name+"/"+contest_name+"_c.py")
-    shutil.copy("template.py", contest_name+"/"+contest_name+"_d.py")
+    shutil.copy(".my_answer/template.py", contest_name+"/"+contest_name+"_a.py")
+    shutil.copy(".my_answer/template.py", contest_name+"/"+contest_name+"_b.py")
+    shutil.copy(".my_answer/template.py", contest_name+"/"+contest_name+"_c.py")
+    shutil.copy(".my_answer/template.py", contest_name+"/"+contest_name+"_d.py")
