3889a68bc473a747b4918f1d0d020faefbc40110
kaikai529 <45989030+kaikai529@users.noreply.github.com>
2022-08-07 20:41:50 +0900

    Example answer (#5)
    * update gitignore
    
    * update 053 and readme
    
    * update abc054

--- example_answer/abc053/abc053_b.py
+++ example_answer/abc053/abc053_b.py
@@ -1,6 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-a = [map(int, input().split()) for _ in range(n)]
+s = input().strip()
+
+a_index = 0
+for i in range(len(s)):
+    if s[i]=="A":
+        a_index = i
+        break
+
+z_index = 0
+for i in range(len(s)-1,-1,-1):
+    if s[i]=="Z":
+        z_index = i
+        break
+
+print(z_index-a_index+1)
