4905c8ef6361e5568cd1a04c2772c77844a9b2b7
kaikai529 <kaishi.19990513@icloud.com>
2022-09-25 21:11:45 +0900

    abc063

--- example_answer/abc063/abc063_b.py
+++ example_answer/abc063/abc063_b.py
@@ -15,10 +15,10 @@ def IN(trans_func=lambda x: x):
     input_ = input().strip().split()
     return int(*input_) if len(input_) == 1 else trans_func(map(int, input_))
 
+
 def STR_IN():
     input_ = input().strip().split()
     return input_[0] if len(input_) == 1 else input_
-
 
 def INs(len_n: int, trans_func=list):
     return [trans_func(map(int, input().split())) for _ in range(len_n)]
@@ -28,3 +28,11 @@ def STR_INs(len_n: int):
     return [input().strip() for _ in range(len_n)]
 
 # main
+
+s = STR_IN()
+kinds = set([_s for _s in s])
+
+if len(s)==len(kinds):
+    print("yes")
+else:
+    print("no")
--- example_answer/abc063/abc063_d.py
+++ example_answer/abc063/abc063_d.py
@@ -15,16 +15,22 @@ def IN(trans_func=lambda x: x):
     input_ = input().strip().split()
     return int(*input_) if len(input_) == 1 else trans_func(map(int, input_))
 
-def STR_IN():
-    input_ = input().strip().split()
-    return input_[0] if len(input_) == 1 else input_
 
+def STR_IN():
+    input_ = input().strip()
+    return input_ if len(input_) == 1 else input_.split()
 
 def INs(len_n: int, trans_func=list):
-    return [trans_func(map(int, input().split())) for _ in range(len_n)]
+    return trans_func([IN() for _ in range(len_n)])
 
 
 def STR_INs(len_n: int):
     return [input().strip() for _ in range(len_n)]
 
 # main
+
+n, a, b = IN()
+h = INs(n)
+h.sort()
+
+h[-1]
