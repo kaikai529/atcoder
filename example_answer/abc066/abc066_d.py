14773c06548b79676f3686a2a9b8a836baa41c36
kaikai529 <kaishi.19990513@icloud.com>
2022-09-26 14:29:07 +0900

    abc066

--- example_answer/abc066/abc066_d.py
+++ example_answer/abc066/abc066_d.py
@@ -15,10 +15,8 @@ def combination(n: int, r: int):
 
 
 def IN(trans_func=lambda x: x):
-    input_ = input().split()
-    if trans_func==list:
-        return trans_func(map(int, input_))
-    return int(*input_) if len(input_) == 1 else map(int, input_)
+    input_ = input().strip().split()
+    return int(*input_) if len(input_) == 1 else trans_func(map(int, input_))
 
 
 def STR_IN():
@@ -34,3 +32,6 @@ def STR_INs(len_n: int):
     return [input().strip() for _ in range(len_n)]
 
 # main
+
+
+n, m = IN()
