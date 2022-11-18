a4ad386401a46e9cd9a31346d7eab37cd66a825e
kaikai529 <45989030+kaikai529@users.noreply.github.com>
2022-11-18 16:43:18 +0900

    Example answer (#41)
    * update 057
    
    * update 253 and 264
    
    * update readme
    
    * update readme
    
    * update 252
    
    * update 251
    
    * update 250
    
    * update abc259
    
    * update abc248
    
    * update 247
    
    * abc245, 246, 265, 266, 267
    
    * abc268 and 269
    
    * abc200 and 201 and update template
    
    * updata readme
    
    * abc060
    
    * abc061
    
    * abc062
    
    * abc063
    
    * abc063
    
    * abc064
    
    * update readme
    
    * abc068
    
    * abc069
    
    * abc070
    
    * update template
    
    * abc077
    
    * abc078
    
    * abc079,080
    
    * abc082
    
    * abc083
    
    * abc084_
    
    * abc084
    
    * abc085
    
    * abc086
    
    * abc087
    
    * abc089
    
    * abc090
    
    * abc093
    
    * abc094
    
    * abc095,096
    
    * abc097
    
    * abc098-100
    
    * abc101
    
    * abc102
    
    * abc102
    
    * abc102 update
    
    * abc103
    
    * abc105
    
    * abc106
    
    * abc104
    
    * abc108
    
    * abc109
    
    * abc109
    
    * abc107
    
    * abc110
    
    * abc111
    
    * abc112
    
    * abc113
    
    * abc114
    
    * abc114_c

---
+++ example_answer/abc113/abc113_c.py
@@ -0,0 +1,103 @@
+from bisect import bisect_left, bisect_right
+from itertools import combinations, count, permutations, product
+from math import ceil, factorial, floor, gcd, inf, sqrt
+from collections import Counter, defaultdict
+from os import defpath
+from queue import LifoQueue, Queue
+import sys
+sys.setrecursionlimit(10 ** 7)  # 再起関数の再起上限
+input = sys.stdin.readline
+# 定数
+MOD = 10**9+7
+VEC4 = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+VEC9 = product([-1, 0, 1], [-1, 0, 1])
+# 自作関数
+
+
+def lcm(a, b):
+    # desc: 最小公倍数を求める関数
+    return a*b // gcd(a, b)
+
+
+# 素数数列を返す
+def prime_numbers(max_n):
+    ps = []
+
+    def is_prime(n):
+        for p in ps:
+            if n % p == 0:
+                return False
+            if p ** 2 >= n:
+                break
+        return True
+
+    for n in range(2, max_n+1):
+        if is_prime(n):
+            ps.append(n)
+
+    return ps
+
+# 約数を返す
+
+
+def divisor(n: int):
+    ans = []
+    for i in range(1, ceil(sqrt(n))):
+        if n % i == 0:
+            ans.append(i)
+            ans.append(n//i)
+    return ans
+
+
+def cum(array: list, key=lambda x: x):
+    cum_sum = [key(array[0])]
+    for i in range(len(array)-1):
+        cum_sum.append(cum_sum[-1]+key(array[i+1]))
+    return cum_sum
+
+
+def combination(n: int, r: int):
+    # desc: nCrを求める関数
+    return factorial(n) // factorial(r) // factorial(n - r)
+
+
+def IN(trans_func=lambda x: x):
+    input_ = input().split()
+    if trans_func == list:
+        return trans_func(map(int, input_))
+    return int(*input_) if len(input_) == 1 else map(int, input_)
+
+
+def STR_IN(trans_func=lambda x: x):
+    input_ = input().strip().split()
+    return trans_func(input_[0]) if len(input_) == 1 else input_
+
+
+def INs(len_n: int, trans_func=lambda x: x):
+    return trans_func([IN(trans_func) for _ in range(len_n)])
+
+
+def STR_INs(len_n: int):
+    return [input().strip() for _ in range(len_n)]
+
+
+# main
+n, m = IN()
+city_info = INs(m, list)
+
+## 県ごとに市を振り分ける
+city_map = [[] for _ in range(n+1)]
+for idx, (prefecture, year) in enumerate(city_info):
+    city_map[prefecture].append((year, idx))
+
+ans = []
+for prefecture in range(n+1):
+    # 属する市がない県はパス
+    if not city_map[prefecture]: continue
+    # 県内でソート
+    city_map[prefecture].sort(key=lambda x:x[0])
+    for i, (year, idx) in enumerate(city_map[prefecture]):
+        ans.append((str(prefecture).zfill(6)+str(i+1).zfill(6), idx))
+
+ans.sort(key=lambda x:x[1])
+for num, _ in ans: print(num)
